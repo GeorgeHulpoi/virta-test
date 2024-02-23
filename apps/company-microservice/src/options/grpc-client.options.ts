5553a7943fb543774d29a5b49e48622b2ad8a591
George-Valentin Hulpoi <george.v.hulpoi@gmail.com>
2024-02-23 18:06:15 +0200

    fix: company grpc client options have wrong path in other apps

--- apps/company-microservice/src/options/grpc-client.options.ts
+++ apps/company-microservice/src/options/grpc-client.options.ts
@@ -6,17 +6,6 @@ export const grpcClientOptions: ClientOptions = {
 	options: {
 		url: `localhost:${process.env.PORT || 5000}`,
 		package: 'company',
-		protoPath:
-			process.env.NODE_ENV === 'production'
-				? join(__dirname, 'proto', 'company.proto')
-				: join(
-						__dirname,
-						'..',
-						'..',
-						'..',
-						'..',
-						'proto',
-						'company.proto',
-					),
+		protoPath: join(process.cwd(), 'proto', 'company.proto'),
 	},
 };
