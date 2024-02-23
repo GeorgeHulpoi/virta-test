802626b1d75243a57060a250f638ef94cdcccd4c
George-Valentin Hulpoi <george.v.hulpoi@gmail.com>
2024-02-23 18:15:55 +0200

    fix(company): delete should return empty object in types

--- apps/company-microservice/src/types.ts
+++ apps/company-microservice/src/types.ts
@@ -3,7 +3,7 @@ import type {Observable} from 'rxjs';
 export interface CompanyService {
 	create(payload: CreateCompany): Observable<Company>;
 	update(payload: UpdateCompany): Observable<Company>;
-	delete(payload: FindCompanyById): Observable<Company>;
+	delete(payload: FindCompanyById): Observable<object>;
 }
 
 export interface Company {
